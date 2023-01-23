7521852d592fb0fefe373aed47f2d1f1789f6ad9
Алексей Ипполитов <setgecko@gmail.com>
2023-01-23 13:59:45 +0300

    Выполнил задание 1.5.3

--- Task_1.5.3/Program.cs
+++ Task_1.5.3/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Привет! \nКак Вас зовут?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("\nСколько вам лет?");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nКем вы работаете?");
+            string userWork = Console.ReadLine();
+
+            Console.WriteLine("\nЗравствуйте, {0}.", userName);
+            Console.WriteLine("Вам, {0} лет.", userAge);
+            Console.WriteLine("Вы прирождённый, {0}!", userWork);
         }
     }
 }
