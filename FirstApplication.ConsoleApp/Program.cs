4779f7018cc6250fee55a079f16258862abe727a
Алексей Ипполитов <setgecko@gmail.com>
2023-01-23 17:45:44 +0300

    Внесены изменения в проект FirstApplication.ConsoleApp для проведения практики 1.7 Отладка приложения

--- FirstApplication.ConsoleApp/Program.cs
+++ FirstApplication.ConsoleApp/Program.cs
@@ -6,7 +6,18 @@
         {
             Console.WriteLine("Как Вас зовут?");
             string name = Console.ReadLine();
-            Console.WriteLine("Здравствуйте, {0}", name);
+
+            Console.WriteLine("Сколько Вам лет");
+            string age = Console.ReadLine();
+
+            Greet(name, age);
+
+            Console.WriteLine("Приятно было познакомиться.");
+        }
+        static void Greet(string name, string age)
+        {
+            Console.WriteLine("Здрвствуйте, {0}", name);
+            Console.WriteLine("Мне сообщили, что Вам {0}", age);
         }
     }
 }
