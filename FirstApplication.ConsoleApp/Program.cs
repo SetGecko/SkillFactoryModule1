62130ec3b8c08db6ab7f6ab7246f20834776d55d
Алексей Ипполитов <setgecko@gmail.com>
2023-01-23 12:54:39 +0300

    Добавьте файлы проекта.

---
+++ FirstApplication.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstApplication.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
